1220f3c6974f06aa76f9032235c008688fa38692
favydabs <favourdabit@gmail.com>
2022-06-21 17:48:49 +0100

    third stage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
 
 function App() {
-  const [Count] = useState(2468);
+  const [Count, setCount] = useState(0);
 
   function increase() {
     // Count++;
+    setCount(Count + 1);
 
+  }
+  function decrease() {
+    setCount(Count - 1);
   }
 
   return (
     <div className="container">
       <h1>{Count}</h1>
+      <button onClick={decrease}>-</button>
+
       <button onClick={increase}>+</button>
+
     </div>
   );
 
